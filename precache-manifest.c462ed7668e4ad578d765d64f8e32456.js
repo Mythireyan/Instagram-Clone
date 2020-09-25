a617ad3a9f314195a56e69cc1407840e6855c2f5
Mythireyan <mythireyanp@gmail.com>
2020-09-25 12:11:17 +0530

    Updates

---
+++ precache-manifest.c462ed7668e4ad578d765d64f8e32456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfdb273f7a7c72cd635f34fededa9265",
+    "url": "/Instagram-Clone/index.html"
+  },
+  {
+    "revision": "7ca12904ec1fbd3372cd",
+    "url": "/Instagram-Clone/static/css/main.183f04e0.chunk.css"
+  },
+  {
+    "revision": "8af36775a6c8ef0f2012",
+    "url": "/Instagram-Clone/static/js/2.804284a9.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Instagram-Clone/static/js/2.804284a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca12904ec1fbd3372cd",
+    "url": "/Instagram-Clone/static/js/main.88ded709.chunk.js"
+  },
+  {
+    "revision": "3207a6b5760ef504a884",
+    "url": "/Instagram-Clone/static/js/runtime-main.de5b5b16.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/instagram-clone/precache-manifest.0cf084fa44fbc1107e78332704a2c343.js"
+  "/Instagram-Clone/precache-manifest.c462ed7668e4ad578d765d64f8e32456.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/instagram-clone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Instagram-Clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
