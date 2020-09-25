289a1cf7581f4f0caeb348b5ffd042ba4babff56
Mythireyan <mythireyanp@gmail.com>
2020-09-25 11:54:45 +0530

    Updates

---
+++ precache-manifest.0cf084fa44fbc1107e78332704a2c343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e35da0724806385dc4a98ae4baaa500",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "7a56bb426905425a92bb",
+    "url": "/instagram-clone/static/css/main.183f04e0.chunk.css"
+  },
+  {
+    "revision": "f7b23d8c022c0f87c101",
+    "url": "/instagram-clone/static/js/2.a80dc683.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram-clone/static/js/2.a80dc683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a56bb426905425a92bb",
+    "url": "/instagram-clone/static/js/main.8060f500.chunk.js"
+  },
+  {
+    "revision": "9d73230a1dae251b7590",
+    "url": "/instagram-clone/static/js/runtime-main.8aef135a.js"
+  }
+]);
